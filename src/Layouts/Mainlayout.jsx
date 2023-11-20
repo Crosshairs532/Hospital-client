df39b51cf0dedba32a4f79845153190ba91ffcc1
sahin-dev <sahinsiraj360@gmail.com>
2023-11-20 19:59:54 +0600

    Modify Mainlayout and remove Hosinfo component

--- src/Layouts/Mainlayout.jsx
+++ src/Layouts/Mainlayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Components/Footer/Footer";
-import Headers from "../Pages/Components/Headers/headers";
+import Headers from "../Pages/Components/Headers/Headers";
 import Hosinfo from "../Pages/Components/Hosinfo/Hosinfo";
 const Mainlayout = () => {
     return (
         <div>
-            <Hosinfo></Hosinfo>
+            
             <Headers></Headers>
             <div className=" min-h-screen">
                 <Outlet></Outlet>
