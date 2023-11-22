d7174676b217615f24d32db02f1537199e4b0b67
sahin-dev <sahinsiraj360@gmail.com>
2023-11-22 20:34:35 +0600

    Implement checkout pages

---
+++ src/Pages/Components/CustomInput/CustomInput.jsx
@@ -0,0 +1,36 @@
+import styles from './Style.module.scss';
+
+const labelText = (text, isRequired = false) => (
+    <div style={{ fontWeight: 'bold' }}>
+        {text}
+        {isRequired && <span style={{ color: 'red', marginLeft: '2px' }}>*</span>}
+    </div>
+);
+
+const CustomInput = ({
+    type = 'text',
+    placeholder,
+    label,
+    name,
+    value,
+    setError,
+    onChange,
+    disabled,
+    required,
+}) => (
+    <main>
+        <p style={{ fontSize: '0.9rem', margin: 0 }}>{labelText(label, required)}</p>
+        <input
+            type={type}
+            placeholder={placeholder}
+            label={label}
+            name={name}
+            className={styles.customInput}
+            defaultValue={value && value}
+            onChange={onChange && ((e) => onChange(e))}
+            onFocus={setError && (() => setError(''))}
+        />
+    </main>
+);
+
+export default CustomInput;
---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+
+import img from '../assets/logo.png'
+
+const cartData = [
+    {
+        productId: '1',
+        productName: 'Sony Headphone',
+        productImage: img,
+        productPrice: 1265,
+        productDiscountPrice: 999,
+    },
+    {
+        productId: '2',
+        productName: 'Inxtax Mini 11 Purple',
+        productImage: img,
+        productPrice: 1355,
+        productDiscountPrice: 950,
+    },
+    {
+        productId: '3',
+        productName: 'Inxtax Mini 11 White',
+        productImage: img,
+        productPrice: 1000,
+        productDiscountPrice: 780,
+    },
+    {
+        productId: '4',
+        productName: 'Bedroom Sofa',
+        productImage: img,
+        productPrice: 550,
+        productDiscountPrice: 0,
+    },
+];
+
+export default cartData;
